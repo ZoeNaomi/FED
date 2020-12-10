1d4edaff0137f93bca4a0cc44009c61ec25aaea2
ZoeNaomi <74123804+ZoeNaomi@users.noreply.github.com>
2020-12-10 13:20:32 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,25 @@
 // JavaScript Document
+
+//var navMenu = document.idSelector("menu");
+
+//function showMenu(){
+//	let hetMenu = document.idSelector("menu");
+//	hetMenu.classList.toggle("toonMenu");
+//}
+
+//navMenu.addEventListener("click", showMenu);
+
+
+
+
+
+
+
+//var deButton = document.querySelector("h2");
+
+//function doeFormHeenEnWeer(){
+//	let hetFormulier = document.querySelector("form");
+//	hetFormulier.classList.toggle("toonForm");
+//}
+
+//deButton.addEventListener("click", doeFormHeenEnWeer);
